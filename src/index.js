1dfd47eff41ed0ff76bcddff9b5bc8c4af8cf32d
Igor-Konyaga <konyaga.igor10@gmail.com>
2023-08-26 01:43:17 +0300

    change task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,90 @@
 import SimpleLightbox from 'simplelightbox';
 import Notiflix from 'notiflix';
-import { fetchData } from "./search";
+import { fetchData } from './search';
+
+const formEl = document.querySelector('#search-form');
+const galleryEl = document.querySelector('.gallery');
+const loadMoreEl = document.querySelector('.load-more');
+
+loadMoreEl.style.display = 'none';
+
+let index = 1;
 
 
+formEl.addEventListener('submit', onSearch);
+
+async function onSearch(e) {
+  e.preventDefault();
+  index = 1;
+  const  searchValue = formEl.elements.searchQuery.value;
+
+  try {
+
+    const data = await fetchData(searchValue, index);
+
+    const markup = renderMarkup(data);
+
+	 galleryEl.innerHTML = markup;
+
+	 loadMoreEl.style.display = 'block';
+
+  } catch (error) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+
+}
+
+function renderMarkup(arr) {
+  const markup = arr.map(
+    ({
+      webformatURL,
+      largeImageURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads,
+    }) => {
+      return `<div class="photo-card">
+		<img src="${webformatURL}" alt="${tags}" loading="lazy" />
+		<div class="info">
+		  <p class="info-item">
+			 <b>Likes: ${likes}</b>
+		  </p>
+		  <p class="info-item">
+		  <b>Views: ${views}</b>
+		  </p>
+		  <p class="info-item">
+		  <b>Comments: ${comments}</b>
+		  </p>
+		  <p class="info-item">
+		  <b>Downloads: ${downloads}</b>
+		  </p>
+		  </div>
+		  </div>`;
+    }
+  );
+
+  return markup;
+}
+
+loadMoreEl.addEventListener('click', onLoadMore);
+
+async function onLoadMore (e) {
+	const searchValue = formEl.children[0].value;
+	index += 1;
+
+loadMoreEl.disabled = true;
+
+const data = await fetchData(searchValue, index);
+
+const markup = renderMarkup(data);
+
+galleryEl.insertAdjacentHTML('beforeend', markup);
+
+loadMoreEl.disabled = false;
+
+console.log(index)
+}
--- src/search.js
+++ src/search.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
 
 const API_KEY = '39039706-642a8919457a778e8308a3f03';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export async function fetchData(search) {
-  const data = await axios(
-    `${BASE_URL}?key=${API_KEY}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true`
-  );
-
-  console.log(data);
-  
-  return data;
+export async function fetchData(searchInput, page) {
+		const data = await axios(
+			`${BASE_URL}?key=${API_KEY}&q=${searchInput}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
+		 );
+	
+		 return data.data.hits;
 }
