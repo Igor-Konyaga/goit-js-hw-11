960c1dd4241b9c96298bb1dcf0c473bc2142949b
Igor-Konyaga <konyaga.igor10@gmail.com>
2023-08-27 00:58:27 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,39 +1,54 @@
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
 import { fetchData } from './search';
 
 const formEl = document.querySelector('#search-form');
 const galleryEl = document.querySelector('.gallery');
 const loadMoreEl = document.querySelector('.load-more');
+const resetEl = document.querySelector('.reset');
 
 loadMoreEl.style.display = 'none';
 
 let index = 1;
 
-
 formEl.addEventListener('submit', onSearch);
 
 async function onSearch(e) {
   e.preventDefault();
+
   index = 1;
-  const  searchValue = formEl.elements.searchQuery.value;
+  const searchValue = formEl.elements.searchQuery.value;
 
   try {
-
     const data = await fetchData(searchValue, index);
 
-    const markup = renderMarkup(data);
+    if (data.totalHits === 0) {
+      galleryEl.innerHTML = '';
 
-	 galleryEl.innerHTML = markup;
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
 
-	 loadMoreEl.style.display = 'block';
+      loadMoreEl.style.display = 'none';
 
+      formEl.reset();
+
+      return;
+    }
+
+    const markup = renderMarkup(data.hits).join('');
+
+    galleryEl.innerHTML = markup;
+
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
+    let lightbox = new SimpleLightbox('.gallery a');
+
+    loadMoreEl.style.display = 'block';
   } catch (error) {
-    Notiflix.Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
+    Notiflix.Notify.failure(error.message);
   }
-
 }
 
 function renderMarkup(arr) {
@@ -48,7 +63,7 @@ function renderMarkup(arr) {
       downloads,
     }) => {
       return `<div class="photo-card">
-		<img src="${webformatURL}" alt="${tags}" loading="lazy" />
+		<a href=${largeImageURL}><img class="img" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
 		<div class="info">
 		  <p class="info-item">
 			 <b>Likes: ${likes}</b>
@@ -72,19 +87,31 @@ function renderMarkup(arr) {
 
 loadMoreEl.addEventListener('click', onLoadMore);
 
-async function onLoadMore (e) {
-	const searchValue = formEl.children[0].value;
-	index += 1;
+async function onLoadMore(e) {
+  const searchValue = formEl.children[0].value;
+  index += 1;
 
-loadMoreEl.disabled = true;
+  loadMoreEl.disabled = true;
 
-const data = await fetchData(searchValue, index);
+  const data = await fetchData(searchValue, index);
 
-const markup = renderMarkup(data);
+  const markup = renderMarkup(data.hits).join('');
 
-galleryEl.insertAdjacentHTML('beforeend', markup);
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+  let lightbox = new SimpleLightbox('.gallery a');
 
-loadMoreEl.disabled = false;
+  if (data.totalHits < index * 40 || data.totalHits === index * 40) {
+    loadMoreEl.style.display = 'none';
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
 
-console.log(index)
+  loadMoreEl.disabled = false;
 }
+
+resetEl.addEventListener('click', () => {
+  formEl.reset();
+  loadMoreEl.style.display = 'none';
+  galleryEl.innerHTML = '';
+});
--- src/search.js
+++ src/search.js
@@ -5,9 +5,9 @@ const API_KEY = '39039706-642a8919457a778e8308a3f03';
 const BASE_URL = 'https://pixabay.com/api/';
 
 export async function fetchData(searchInput, page) {
-		const data = await axios(
+		const {data} = await axios(
 			`${BASE_URL}?key=${API_KEY}&q=${searchInput}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
 		 );
 	
-		 return data.data.hits;
+		 return data;
 }
